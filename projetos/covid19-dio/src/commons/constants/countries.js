df45788f2704759cdb7a9e60665b621707d9f1f0
Dari Alves <darieltondf@gmail.com>
2020-10-08 17:28:45 -0300

    pwa com react para mapear dados covid

---
+++ projetos/covid19-dio/src/commons/constants/countries.js
@@ -0,0 +1,59 @@
+import BrazilFlag from '../../assets/images/brazil.png'
+import ChinaFlag from '../../assets/images/china.png'
+import GermanyFlag from '../../assets/images/germany.png'
+import ItalyFlag from '../../assets/images/italy.png'
+import UsaFlag from '../../assets/images/usa.png'
+import ArgentinaFlag from '../../assets/images/argentina.png'
+import KoreaFlag from '../../assets/images/korea.png'
+import JapanFlag from '../../assets/images/japan.png'
+import World from '../../assets/images/world.png'
+
+const COUNTRIES = [
+  {
+    label: 'Mundo',
+    value: 'world',
+    flag: World
+  },
+  {
+    label: 'Alemanha',
+    value: 'germany',
+    flag: GermanyFlag
+  },
+  {
+    label: 'Brasil',
+    value: 'brazil',
+    flag: BrazilFlag
+  },
+  {
+    label: 'China',
+    value: 'china',
+    flag: ChinaFlag
+  },
+  {
+    label: 'Estados Unidos',
+    value: 'usa',
+    flag: UsaFlag
+  },
+  {
+    label: 'Itália',
+    value: 'italy',
+    flag: ItalyFlag
+  },
+  {
+    label: 'Argentina',
+    value: 'argentina',
+    flag: ArgentinaFlag
+  },
+  {
+    label: 'Coreia do Sul',
+    value: 's. korea',
+    flag: KoreaFlag
+  },
+  {
+    label: 'Japão',
+    value: 'japan',
+    flag: JapanFlag
+  }
+]
+
+export default COUNTRIES
